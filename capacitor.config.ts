efdc0a3b8b544a5c3af509a289ce688d7ff50484
Tomás Durán <101905549+Tomas-Leiva-Duran@users.noreply.github.com>
2024-01-18 15:08:02 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.calculadorapromedio004',
+  appName: 'calculadorapromedio004',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
